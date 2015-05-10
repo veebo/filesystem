5a297b11569829256867f388b0d7a5cd44e48bf4
vanWinkle <kolja1304@yndex.ru>
2015-05-10 15:15:58 +0300

    Update Oglavlenie_poAlfavity.cpp

--- filesystem/Oglavlenie_poAlfavity.cpp
+++ filesystem/Oglavlenie_poAlfavity.cpp
@@ -4,14 +4,25 @@
 void List(FileSystem *fs, int argc, char *argv[], ostream& out){
 //будет примерно так
   FileIterator* fi=fs->GetIterator();
-  char* spisok=new char[15*(fi->GetFilesCount())];
-  spisok='';
+  char fName[20];
+  char** spisok=new fName[(fi->GetFilesCount())];
   for(int i=0;i<fi->GetFilesCount();i++){
     FileDescriptor* fd=fi->GetFileDescriptor();
-    spisok=spisok+fd->GetName()+'\n';
+    spisok[i]=fd->GetName();
     fi->Next();
   }
-  //здесь алгоритм сортировки
+  TempfName=spisok[0];
+  int k=-1;
+  for(int i=0;i<fi->GetFilesCount();i++){
+    if(TempfName>spisok[i]){
+      TempfName=spisok[i];
+      k=i;
+    }
+  }
+  if(k>=0){
+    spisok[k]='{';
+    out<<TempfName;
+  }
   
 spisok=spisok+'\0';
 out<<spisok;
