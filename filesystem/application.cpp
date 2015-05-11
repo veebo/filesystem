b9aa5f6e5d418c80b2ff284e40429681eb326bee
vanWinkle <kolja1304@yndex.ru>
2015-05-11 21:03:45 +0300

    Update application.cpp

--- filesystem/application.cpp
+++ filesystem/application.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "Monitor.h"
+#include <locale>
 #include <conio.h>
 
 
 int main(){
+	setlocale(LC_ALL,"Russian");
 	InputOutput* io = new InputOutput(std::cin, std::cout);
 	Monitor* monitor = new Monitor(io);
 	std::vector<char*>* input;
